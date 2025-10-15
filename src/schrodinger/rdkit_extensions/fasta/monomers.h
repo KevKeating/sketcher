7d0f10ae38ab1a6fb89fded32141f5da18e8560c
Kevin Keating <kevin.keating@schrodinger.com>
2025-10-15 17:04:12 -0400

    adding missing include

--- src/schrodinger/rdkit_extensions/fasta/monomers.h
+++ src/schrodinger/rdkit_extensions/fasta/monomers.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <optional>
+#include <string>
 #include <string_view>
 
 namespace fasta
@@ -15,6 +16,6 @@ get_fasta_to_helm_nucleotide(char one_letter_monomer,
 [[nodiscard]] std::optional<char>
 get_helm_to_fasta_amino_acid(const std::string& helm_amino_acid);
 
-[[nodiscard]] std::optional<char>
+[[nodiscard]] std::optional<char>QT_FEATURE_system_jpeg
 get_helm_to_fasta_nucleotide(const std::string& helm_nucleotide);
 } // namespace fasta
