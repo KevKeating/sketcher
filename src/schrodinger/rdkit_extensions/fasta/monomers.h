a00838741b8addc230f8be47fc23619f1302d66e
Kevin Keating <kevin.keating@schrodinger.com>
2025-10-15 17:06:25 -0400

    fixing errant paste

--- src/schrodinger/rdkit_extensions/fasta/monomers.h
+++ src/schrodinger/rdkit_extensions/fasta/monomers.h
@@ -16,6 +16,6 @@ get_fasta_to_helm_nucleotide(char one_letter_monomer,
 [[nodiscard]] std::optional<char>
 get_helm_to_fasta_amino_acid(const std::string& helm_amino_acid);
 
-[[nodiscard]] std::optional<char>QT_FEATURE_system_jpeg
+[[nodiscard]] std::optional<char>
 get_helm_to_fasta_nucleotide(const std::string& helm_nucleotide);
 } // namespace fasta
