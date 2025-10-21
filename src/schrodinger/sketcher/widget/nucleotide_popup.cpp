6a52b74447b91aa65ee8e14fe6c3368c3c866ce2
Kevin Keating <kevin.keating@schrodinger.com>
2025-10-21 11:02:41 -0400

    adding files that I forgot to add

---
+++ src/schrodinger/sketcher/widget/nucleotide_popup.cpp
@@ -0,0 +1,84 @@
+#include "schrodinger/sketcher/ui/ui_nucleotide_popup.h"
+
+#include <QButtonGroup>
+
+#include "schrodinger/sketcher/widget/nucleotide_popup.h"
+#include "schrodinger/sketcher/model/sketcher_model.h"
+
+namespace schrodinger
+{
+namespace sketcher
+{
+
+NucleotidePopup::NucleotidePopup(const NucleicAcidTool tool, const ModelKey model_key, const QString& sugar, const QString& u_or_t,
+                                 QWidget* parent) :
+    m_tool(tool),
+    m_model_key(model_key),
+    m_sugar(sugar),
+    m_u_or_t(u_or_t),
+    ModularPopup(parent)
+{
+    ui.reset(new Ui::NucleotidePopup());
+    ui->setupUi(this);
+    setButtonGroup(ui->group);
+    
+    // TODO: put correct text into u_or_t_btn
+}
+
+NucleotidePopup::~NucleotidePopup()
+{
+}
+
+int NucleotidePopup::getButtonIDToCheck()
+{
+    auto model = getModel();
+    if (model == nullptr) {
+        return -1;
+    }
+
+    int button_id = -1;
+    if (model->getDrawTool() == DrawTool::MONOMER &&
+        model->getMonomerToolType() == MonomerToolType::NUCLEIC_ACID &&
+        model->getNucleicAcidTool() == m_tool) {
+        StdNucleobase base = model->getValue(m_model_key).value<StdNucleobase>();
+        switch (base) {
+            case StdNucleobase::A:
+                button_id = m_group->id(ui->a_btn);
+                break;
+            case StdNucleobase::C:
+                button_id = m_group->id(ui->c_btn);
+                break;
+            case StdNucleobase::G:
+                button_id = m_group->id(ui->g_btn);
+                break;
+            case StdNucleobase::U_OR_T:
+                button_id = m_group->id(ui->u_or_t_btn);
+                break;
+            case StdNucleobase::N:
+                button_id = m_group->id(ui->n_btn);
+                break;
+            default:
+                break;
+        }
+    }
+    return button_id;
+}
+
+void NucleotidePopup::generateButtonPackets()
+{
+    m_button_packets.emplace_back(ui->a_btn,
+                                  static_cast<int>(StdNucleobase::A));
+    m_button_packets.emplace_back(ui->c_btn,
+                                  static_cast<int>(StdNucleobase::C));
+    m_button_packets.emplace_back(ui->g_btn,
+                                  static_cast<int>(StdNucleobase::G));
+    m_button_packets.emplace_back(ui->u_or_t_btn,
+                                  static_cast<int>(StdNucleobase::U_OR_T));
+    m_button_packets.emplace_back(ui->n_btn,
+                                  static_cast<int>(StdNucleobase::N));
+}
+
+} // namespace sketcher
+} // namespace schrodinger
+
+#include "schrodinger/sketcher/widget/nucleotide_popup.moc"
---
+++ src/schrodinger/sketcher/widget/nucleotide_popup.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <memory>
+
+#include "schrodinger/sketcher/definitions.h"
+#include "schrodinger/sketcher/widget/modular_popup.h"
+
+class QString;
+
+namespace Ui
+{
+class NucleotidePopup;
+}
+
+namespace schrodinger
+{
+namespace sketcher
+{
+
+enum class NucleicAcidTool;
+enum class ModelKey;
+
+/**
+ * Popup used to provide functionality choices for a selection tool button.
+ */
+class SKETCHER_API NucleotidePopup : public ModularPopup
+{
+    Q_OBJECT
+
+  public:
+    NucleotidePopup(const NucleicAcidTool tool, const ModelKey model_key,
+                    const QString& sugar, const QString& u_or_t,
+                    QWidget* parent = nullptr);
+    ~NucleotidePopup();
+
+  protected:
+    NucleicAcidTool m_tool;
+    ModelKey m_model_key;
+    QString m_sugar;
+    QString m_u_or_t;
+
+    std::unique_ptr<Ui::NucleotidePopup> ui;
+    void generateButtonPackets() override;
+    int getButtonIDToCheck() override;
+};
+
+} // namespace sketcher
+} // namespace schrodinger
