dc537b3252d5f398f0de661995ec81a0602d39d6
Kevin Keating <kevin.keating@schrodinger.com>
2026-01-16 13:59:46 -0500

    test now passes

--- test/schrodinger/sketcher/rdkit/test_monomeric.cpp
+++ test/schrodinger/sketcher/rdkit/test_monomeric.cpp
@@ -45,8 +45,8 @@ BOOST_AUTO_TEST_CASE(test_contains_two_monomer_linkages)
 
 BOOST_AUTO_TEST_CASE(test_get_attachment_points)
 {
-    std::unordered_set<std::string> exp_names;
     std::unordered_map<std::string, const RDKit::Atom*> exp_bound;
+    std::unordered_set<std::string> exp_available;
     const RDKit::Atom *atom0, *atom1, *atom2;
     
     // a lone alanine has no bound attachment points
@@ -54,26 +54,25 @@ BOOST_AUTO_TEST_CASE(test_get_attachment_points)
     {
         atom0 = mol->getAtomWithIdx(0);
         BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom0).empty());
-        exp_names = {"N", "C", "X"};
-        BOOST_TEST(get_available_attachment_point_names(atom0) == exp_names);
+        exp_available = {"N", "C", "X"};
+        BOOST_TEST(get_available_attachment_point_names(atom0) == exp_available);
     }
 
     // two alanines next to each other
     mol = rdkit_extensions::to_rdkit("PEPTIDE1{A.A}$$$$V2.0");
     {
-        // the first alanine has a bound C-terminus
         const auto* atom0 = mol->getAtomWithIdx(0);
         const auto* atom1 = mol->getAtomWithIdx(1);
+
         exp_bound = {{"C", atom1}};
         BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom0) == exp_bound);
-        exp_names = {"N", "X"};
-        BOOST_TEST(get_available_attachment_point_names(atom0) == exp_names);
+        exp_available = {"N", "X"};
+        BOOST_TEST(get_available_attachment_point_names(atom0) == exp_available);
 
-        // the second alanine has a bound N-terminus
         exp_bound = {{"N", atom0}};
         BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom1) == exp_bound);
-        exp_names = {"C", "X"};
-        BOOST_TEST(get_available_attachment_point_names(atom1) == exp_names);
+        exp_available = {"C", "X"};
+        BOOST_TEST(get_available_attachment_point_names(atom1) == exp_available);
     }
 
     // a side-chain interaction between two non-adjacent residues
@@ -84,40 +83,39 @@ BOOST_AUTO_TEST_CASE(test_get_attachment_points)
         atom1 = mol->getAtomWithIdx(1);
         atom2 = mol->getAtomWithIdx(2);
         
-        exp_bound = {{"C": atom1, "X"};
+        exp_bound = {{"C", atom1}, {"X", atom2}};
         BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom0) == exp_bound);
-        exp_names = {"N"};
-        BOOST_TEST(get_available_attachment_point_names(atom0) == exp_names);
+        exp_available = {"N"};
+        BOOST_TEST(get_available_attachment_point_names(atom0) == exp_available);
 
-        exp_bound = {1, 2};
+        exp_bound = {{"N", atom0}, {"C", atom2}};
         BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom1) == exp_bound);
-        exp_names = {"X"};
-        BOOST_TEST(get_available_attachment_point_names(atom1) == exp_names);
+        exp_available = {"X"};
+        BOOST_TEST(get_available_attachment_point_names(atom1) == exp_available);
 
-        exp_bound = {1, 3};
+        exp_bound = {{"N", atom1}, {"X", atom0}};
         BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom2) == exp_bound);
-        exp_names = {"C"};
-        BOOST_TEST(get_available_attachment_point_names(atom2) == exp_names);
+        exp_available = {"C"};
+        BOOST_TEST(get_available_attachment_point_names(atom2) == exp_available);
     }
 
-    // // a side-chain interaction between two adjacent residues
-    // mol = rdkit_extensions::to_rdkit(
-    //     "PEPTIDE1{C.C}$PEPTIDE1,PEPTIDE1,1:R3-2:R3$$$V2.0");
-    // atom = mol->getAtomWithIdx(0);
-    // exp_bound = {2, 3};
-    // BOOST_TEST(get_bound_attachment_points(atom) == exp_bound);
-    // exp_available = {1};
-    // BOOST_TEST(get_available_attachment_points(atom) == exp_available);
-    // exp_names = {"N"};
-    // BOOST_TEST(get_available_attachment_point_names(atom) == exp_names);
+    // a side-chain interaction between two adjacent residues
+    mol = rdkit_extensions::to_rdkit(
+        "PEPTIDE1{C.C}$PEPTIDE1,PEPTIDE1,1:R3-2:R3$$$V2.0");
+    {
+        atom0 = mol->getAtomWithIdx(0);
+        atom1 = mol->getAtomWithIdx(1);
 
-    // atom = mol->getAtomWithIdx(1);
-    // exp_bound = {1, 3};
-    // BOOST_TEST(get_bound_attachment_points(atom) == exp_bound);
-    // exp_available = {2};
-    // BOOST_TEST(get_available_attachment_points(atom) == exp_available);
-    // exp_names = {"C"};
-    // BOOST_TEST(get_available_attachment_point_names(atom) == exp_names);
+        exp_bound = {{"C", atom1}, {"X", atom1}};
+        BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom0) == exp_bound);
+        exp_available = {"N"};
+        BOOST_TEST(get_available_attachment_point_names(atom0) == exp_available);
+
+        exp_bound = {{"N", atom0}, {"X", atom0}};
+        BOOST_TEST(get_bound_attachment_point_names_and_bound_atoms(atom1) == exp_bound);
+        exp_available = {"C"};
+        BOOST_TEST(get_available_attachment_point_names(atom1) == exp_available);
+    }
     
     // TODO: test CHEM monomers
     // TODO: test NA_PHOSPHATE monomers
