bb63d8ea2f78f3e71cb74b7c2ea0b2a64a4f1a3e
Kevin Keating <kevin.keating@schrodinger.com>
2026-03-18 08:48:20 -0400

    Revert "reverting MolModel changes - should reapply on a new branch"
    This reverts commit 6147976983d2d39aca8d79635ec4326506370950.

--- src/schrodinger/sketcher/model/mol_model.cpp
+++ src/schrodinger/sketcher/model/mol_model.cpp
@@ -650,11 +650,22 @@ void MolModel::addAttachmentPoint(const RDGeom::Point3D& coords,
                             WhatChanged::MOLECULE);
 }
 
-// TODO: add bound_to_atom param
+// TODO: add linkage param
 void MolModel::addMonomer(const std::string_view res_name,
                           const rdkit_extensions::ChainType chain_type,
-                          const RDGeom::Point3D& coords)
+                          const RDGeom::Point3D& coords, const RDKit::Atom* const bound_to_monomer)
 {
+    // TODO: create separate addMonomer and addBoundMonomer methods?  Other than
+    //       create_atom function, they won't actually overlap much
+    // TODO: need to worry about adding whole fragment at once for nucleotide
+    //       tools (need to tag all incoming monomers and connections)
+    // TODO: if this is going to be bound to an existing monomer, get chain_id
+    //       from that monomer and figure out the correct residue number
+    // TODO: if bound, need to add tag to new bond
+    // TODO: may need to swap linkage and bond direction so that the higher
+    //       number attachment point is first
+    
+    
     // we'll renumber the chains in assignChains, so for now we just need
     // something with the correct prefix and a unique number
     auto chain_id = rdkit_extensions::toString(chain_type) + "999999";
@@ -666,10 +677,10 @@ void MolModel::addMonomer(const std::string_view res_name,
         set_atom_monomeric(monomer.get());
         return monomer;
     };
-    auto bound_to_atom_tag = getTagForAtom(nullptr, true);
+    auto bound_to_atom_tag = getTagForAtom(bound_to_monomer, true);
     auto cmd_func = [this, create_atom, coords, bound_to_atom_tag]() {
         addAtomChainCommandFunc(create_atom, {coords}, make_new_single_bond,
-                                bound_to_atom_tag);
+                                AtomTag(-1));
         rdkit_extensions::assignChains(m_mol);
     };
     doCommandUsingSnapshots(cmd_func, "Add monomer", WhatChanged::MOLECULE);
