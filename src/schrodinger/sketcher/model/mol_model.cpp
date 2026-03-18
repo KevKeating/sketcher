8152bdd5d3310b423ad9e49c153322a49bc34f96
Kevin Keating <kevin.keating@schrodinger.com>
2026-03-18 11:49:06 -0400

    using rdkit_extensions::ChainType

--- src/schrodinger/sketcher/model/mol_model.cpp
+++ src/schrodinger/sketcher/model/mol_model.cpp
@@ -690,9 +690,9 @@ static int get_residue_number_for_new_monomer(const std::string_view res_name,
     using rdkit_extensions::ChainType;
     int ideal_res_num_offset = 1;
     auto bound_to_monomer_chain_type = rdkit_extensions::getChainType(*bound_to_monomer);
-    if (chain_type == rdkit_extensions::ChainType::PEPTIDE && bound_to_monomer_chain_type == rdkit_extensions::ChainType::PEPTIDE && new_monomer_ap_name == ap_model_name_for(PeptideAP::N)) {
+    if (chain_type == ChainType::PEPTIDE && bound_to_monomer_chain_type == ChainType::PEPTIDE && new_monomer_ap_name == ap_model_name_for(PeptideAP::N)) {
         ideal_res_num_offset = -1;
-    } else if (chain_type == rdkit_extensions::ChainType::RNA && bound_to_monomer_chain_type == rdkit_extensions::ChainType::RNA) {
+    } else if (chain_type == ChainType::RNA && bound_to_monomer_chain_type == ChainType::RNA) {
         auto monomer_type = get_na_monomer_type_from_res_name(res_name);
         if (monomer_type == MonomerType::NA_SUGAR && new_monomer_ap_name == ap_model_name_for(NASugarAP::THREE_PRIME)) {
             // sugars can link to the previous residue
