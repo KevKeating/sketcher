2b96659a2a09469df176e98a0acdfe2b0a93f8bf
Kevin Keating <kevin.keating@schrodinger.com>
2026-07-07 14:15:20 -0400

    adding docstrings

--- src/app/image_generation_from_js.h
+++ src/app/image_generation_from_js.h
@@ -1,3 +1,7 @@
+/**
+ * Helper functions for calling the image_generation API from JavaScript
+ */
+
 #pragma once
 
 #ifdef __EMSCRIPTEN__
@@ -8,7 +12,15 @@
 
 #include "schrodinger/sketcher/image_generation.h"
 
+/**
+ * Convert a QByteArray to a JavaScript array of uint8 values
+ */
 emscripten::val qbyte_array_to_uint8_array(QByteArray bytes);
+
+/**
+ * Convert a JavaScript object that describes render options to an actual C++
+ * RenderOptions instance.
+ */
 schrodinger::sketcher::RenderOptions
 render_options_from_js(const emscripten::val& options);
 
