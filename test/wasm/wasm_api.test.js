6c74133527673efe26d12a07b881faaca1190b2d
Kevin Keating <kevin.keating@schrodinger.com>
2026-07-14 12:31:36 -0400

    Codex fixing PNG test update

--- test/wasm/wasm_api.test.js
+++ test/wasm/wasm_api.test.js
@@ -150,7 +150,9 @@ test.describe('WASM Sketcher API', () => {
         const svg = Buffer.from(bytes).toString('utf8');
         expect(svg).toContain('<svg');
       } else {
-        expect(bytes.slice(0, 8)).toEqual([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]);
+        expect(Array.from(bytes.subarray(0, 8))).toEqual([
+          0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a,
+        ]);
       }
     });
   });
