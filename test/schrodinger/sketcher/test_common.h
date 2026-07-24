79ede750019f9a5586ef2b4f68054ebcf4fadc5e
Kevin Keating <kevin.keating@schrodinger.com>
2026-07-24 13:36:44 -0400

    clang-format

--- test/schrodinger/sketcher/test_common.h
+++ test/schrodinger/sketcher/test_common.h
@@ -70,7 +70,8 @@ MAKE_ENUM_LOGGABLE(SelectionTool)
 class TestSketcherWidget : public SketcherWidget
 {
   public:
-    TestSketcherWidget() : SketcherWidget(){
+    TestSketcherWidget() : SketcherWidget()
+    {
         setAttribute(Qt::WA_DontShowOnScreen);
     };
     using SketcherWidget::addFromString;
