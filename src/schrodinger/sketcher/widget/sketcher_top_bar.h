2de30eb0cb1f9b7bd48b110605074a0e0f56bcad
Kevin Keating <kevin.keating@schrodinger.com>
2026-09-11 11:44:18 -0400

    Codex implementing new menu option

--- src/schrodinger/sketcher/widget/sketcher_top_bar.h
+++ src/schrodinger/sketcher/widget/sketcher_top_bar.h
@@ -116,6 +116,9 @@ class SKETCHER_API SketcherTopBar : public SketcherView
      */
     void onImportFromFileClicked();
 
+    /** Open a JSON file and load its custom monomer definitions. */
+    void onLoadMonomerDatabaseClicked();
+
     /**
      * Respond to a click on the "Paste in Text..." action.
      *
