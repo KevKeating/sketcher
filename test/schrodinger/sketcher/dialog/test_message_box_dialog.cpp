c3ed45cf7b9a9f68c31f14b311f226637b721cb7
Kevin Keating <kevin.keating@schrodinger.com>
2026-09-25 10:03:54 -0400

    Codex adding more tests to test_message_box_dialog.cpp

--- test/schrodinger/sketcher/dialog/test_message_box_dialog.cpp
+++ test/schrodinger/sketcher/dialog/test_message_box_dialog.cpp
@@ -28,5 +28,33 @@ BOOST_AUTO_TEST_CASE(test_show_warning_dialog)
     BOOST_TEST(button_box->standardButtons().testFlag(QDialogButtonBox::Cancel));
 }
 
+BOOST_AUTO_TEST_CASE(test_show_error_dialog)
+{
+    QWidget parent;
+
+    show_error_dialog("Error", "Error text", &parent);
+
+    auto* dialog = parent.findChild<MessageBoxDialog*>();
+    BOOST_REQUIRE(dialog != nullptr);
+    auto* button_box = dialog->findChild<QDialogButtonBox*>("button_box");
+    BOOST_REQUIRE(button_box != nullptr);
+    BOOST_TEST(button_box->standardButtons().testFlag(QDialogButtonBox::Ok));
+    BOOST_TEST(!button_box->standardButtons().testFlag(QDialogButtonBox::Cancel));
+}
+
+BOOST_AUTO_TEST_CASE(test_show_information_dialog)
+{
+    QWidget parent;
+
+    show_information_dialog("Information", "Information text", &parent);
+
+    auto* dialog = parent.findChild<MessageBoxDialog*>();
+    BOOST_REQUIRE(dialog != nullptr);
+    auto* button_box = dialog->findChild<QDialogButtonBox*>("button_box");
+    BOOST_REQUIRE(button_box != nullptr);
+    BOOST_TEST(button_box->standardButtons().testFlag(QDialogButtonBox::Ok));
+    BOOST_TEST(!button_box->standardButtons().testFlag(QDialogButtonBox::Cancel));
+}
+
 } // namespace sketcher
 } // namespace schrodinger
